87b33b02483f2c7c1bda08350aa7de671b5bd2cb
galle2018 <hfsoluciones@hotmail.es>
2018-07-22 10:25:40 -0300

    Modificaciones en los modulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,21 +2,16 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { PagesComponent } from './pages/pages.component';
 
-import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+
 import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from "./login/register.component";
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { RegisterComponent } from './login/register.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 const appRoutes: Routes = [
-  {path: '', component: PagesComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent },
-  {path: 'progress', component: ProgressComponent},
-  {path: 'graficas1', component: Graficas1Component},
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  {path: '**', component: NopagefoundComponent}
+  {path: 'register', component: RegisterComponent},
+  { path: '**', component: NopagefoundComponent } // cualquier otra ruta direciona al dashboard
 ];
 
 export const APP_ROUTES = RouterModule.forRoot (appRoutes, {useHash: true});
+// uso el forRoot pues es nuestra rura principal. forChild es para rutas hijas
